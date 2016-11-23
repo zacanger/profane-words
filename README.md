d5033e6a6be298ee568485b063a151fe33126ee4
zacanger <zac@zacanger.com>
2016-11-23 16:41:16 -0700

    words

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 --------
 
-Installation:
+Installation: `npm i -S profane-words`
 
 Usage:
+```javascript
+import { de, en, es, fr, it, nl, pt, ru } from 'profane-words'
+
+console.log(en[0])
+```
+
+Why: blacklisting words, I guess.
 
 License: WTFPL
---
+++ index.js
@@ -0,0 +1,20 @@
+const
+  de = require('./de')
+, en = require('./en')
+, es = require('./es')
+, fr = require('./fr')
+, it = require('./it')
+, nl = require('./nl')
+, pt = require('./pt')
+, ru = require('./ru')
+
+module.exports = {
+  de
+, en
+, es
+, fr
+, it
+, nl
+, pt
+, ru
+}
